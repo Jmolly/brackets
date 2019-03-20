1d54856fbcf822b16ac879fe4f6f29de2cdf5558
Jmolly <roshkovskypetr@gmail.com>
2019-03-20 22:53:35 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+  const strElements = str.split("");
+  let result;
+
+  while (strElements.length > 0) {
+    result = false;
+    for (let i = 0, strLength = strElements.length; i < strLength; i++) {
+      for (let j = 0, bracketConfLength = bracketsConfig.length; j < bracketConfLength; j++)
+        if (strElements[i] === bracketsConfig[j][0] && strElements[i + 1] === bracketsConfig[j][1]) {
+          strElements.splice(i, 2);
+          result = true;
+        }
+    }
+    if (!result) break;
+  }
+  
+  return result;
 }
